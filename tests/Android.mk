9db573ad363f24bd63fc6d440204d82a6df5e3ef
George G <kreach3r@users.noreply.github.com>
2016-11-18 15:08:24 +0100

    tests: refactor into KatsunaSetupWizardtests
    We still need to find out how to use them

--- tests/Android.mk
+++ tests/Android.mk
@@ -5,13 +5,10 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_PRIVILEGED_MODULE := true
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.cyanogenmod.platform.sdk
-
 # Include all test java files.
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := CyanogenSetupWizardTests
+LOCAL_PACKAGE_NAME := KatsunaSetupWizardTests
 LOCAL_CERTIFICATE := platform
 
 include $(BUILD_PACKAGE)
