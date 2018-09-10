934f7c610da424e5b89c93f249334021c0a875a5
George G <kreach3r@gmail.com>
2018-09-10 14:41:06 +0300

    Android.mk: use AAPT2

--- Android.mk
+++ Android.mk
@@ -1,8 +1,7 @@
 LOCAL_PATH := $(call my-dir)
-KATSUNA_COMMON_PATH := $(ANDROID_BUILD_TOP)/frameworks/KatsunaCommon
 include $(CLEAR_VARS)
 
-LOCAL_FULL_LIBS_MANIFEST_FILES := $(KATSUNA_COMMON_PATH)/commons/src/main/AndroidManifest.xml
+LOCAL_USE_AAPT2 := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
@@ -14,34 +13,25 @@ LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
+LOCAL_STATIC_ANDROID_LIBRARIES := \
+    KatsunaCommon \
     android-support-v4 \
     android-support-v7-cardview \
     android-support-v7-recyclerview \
     android-support-v13 \
     android-support-design
 
-LOCAL_JAVA_LIBRARIES := telephony-common
+LOCAL_JAVA_LIBRARIES := \
+    telephony-common
 
-LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, res) \
-    frameworks/support/v7/appcompat/res \
-    frameworks/support/v7/cardview/res \
-    frameworks/support/v7/recyclerview/res \
-    frameworks/support/design/res
+LOCAL_RESOURCE_DIR := \
+    $(addprefix $(LOCAL_PATH)/, res)
 
-LOCAL_STATIC_JAVA_AAR_LIBRARIES := roundedimageview
+LOCAL_STATIC_JAVA_AAR_LIBRARIES := \
+    roundedimageview
 
 # Include KatsunaCommon into this app
 LOCAL_REQUIRED_MODULES := KatsunaCommon
-LOCAL_STATIC_JAVA_LIBRARIES += KatsunaCommon
-# Include KatsunaCommon resources
-LOCAL_RESOURCE_DIR += frameworks/KatsunaCommon/commons/src/main/res
-
-LOCAL_AAPT_FLAGS := --auto-add-overlay \
-    --extra-packages android.support.v7.appcompat \
-    --extra-packages android.support.v17.preference \
-    --extra-packages com.katsuna.commons \
-    --extra-packages com.makeramen.roundedimageview
 
 include frameworks/opt/setupwizard/library/common.mk
 
